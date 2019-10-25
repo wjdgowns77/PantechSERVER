ccdea547ecf30a3e3d92941ce1ad5a3931f8eee8
wjdgowns77 <31847906+wjdgowns77@users.noreply.github.com>
2019-10-25 10:10:27 +0900

    잘못된 디렉터리에 올려서 다시 올립니다...

---
+++ apkmanager.vegaservice.co.kr/apkmanager/Process/sky_station_30_server(2).php
@@ -0,0 +1,11 @@
+--pkgname
+Content-Disposition:form-data;name='file';filename=GET_PKG_DETAIL_INFO.xml
+
+<?xml version="1.0"?>
+<SKY_REQUEST>
+  <SKY_CMD>GET_PKG_DETAIL_INFO</SKY_CMD>
+  <TERMINAL_NAME>IM-A840S</TERMINAL_NAME>
+  <BOARD_SOFT_VER>S0215</BOARD_SOFT_VER>
+  <PKG_NAME>com.pantech.firmware.bin.IM-A840S</PKG_NAME>
+</SKY_REQUEST>
+--pkgname--
